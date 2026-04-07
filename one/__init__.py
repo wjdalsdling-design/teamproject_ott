de13f1e8d90b3112c92e703a0dccbee05612d4dc
KangGyungGheun <carrotkang@gmail.com>
2026-04-07 17:42:34 +0900

    frame작업

--- one/__init__.py
+++ one/__init__.py
@@ -1,12 +1,18 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
-
+import config
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app=Flask(__name__)
+    app.config.from_object(config)
 
-    @app.route('/')
-        def hello_pybo():
-            return 'Hello 4flex!'
+    # ORM
+    db.init_app(app)
+    migrate.init_app(app, db)
+    from . import models
 
     return app
